27daff69f252629d3d09872ec6d3eb683876dffd
Tiffceet <zkang.loo@gmail.com>
2021-07-02 13:37:42 +0800

    Make it possible to run in external window

--- src/main/java/MainDriver/MainClass.java
+++ src/main/java/MainDriver/MainClass.java
@@ -1,7 +1,8 @@
 package MainDriver;
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("I just ran");
+        new java.util.Scanner(System.in).nextLine();
     }
 }
