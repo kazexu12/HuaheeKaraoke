527cbf8cc55759ec263c772a9b5e828eea5e3b2d
Tiffceet <zkang.loo@gmail.com>
2021-07-08 14:23:35 +0800

    Add: log4j 2 logging framework

--- src/main/java/MainDriver/MainClass.java
+++ src/main/java/MainDriver/MainClass.java
@@ -3,18 +3,54 @@ package MainDriver;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 import Generic.DBManager;
+import java.io.IOException;
+import java.lang.Thread.UncaughtExceptionHandler;
+import java.sql.SQLException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MainClass {
 
-    public static void main(String[] args) throws Exception {
+    private static final Logger logger = LogManager.getLogger(MainClass.class.getName());
+
+    public static void init() {
         // Initializing DB
-        DBManager db = new DBManager();
-        db.prepareTable();
-        
+        try {
+            DBManager db = new DBManager();
+            db.prepareTable();
+        } catch (SQLException err) {
+            logger.error("Unable to create database", err);
+        }
+
         // Initialising terminal
-        Terminal terminal = TerminalBuilder.builder()
-                .system(true).jna(true).type("windows-vtp")
-                .build();
-        terminal.enterRawMode();
-        Playground.charInputPlayground(terminal);
+        try {
+            Terminal terminal = TerminalBuilder.builder()
+                    .system(true).jna(true).type("windows-vtp")
+                    .build();
+            terminal.enterRawMode();
+            Playground.charInputPlayground(terminal);
+        } catch (IOException err) {
+            logger.error("Unable to initialise terminal", err);
+        }
+    }
+
+    /**
+     * Self-made class to capture uncaught exceptions
+     */
+    static class ErrorHandler implements UncaughtExceptionHandler {
+
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            // Logs the exception using log4j 2
+            logger.error("Uncaught exception in thread: " + t.getName(), e);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Handle uncaught exceptions
+        Thread.setDefaultUncaughtExceptionHandler(new ErrorHandler());
+
+        init();
     }
 }
--- src/main/java/MainDriver/Playground.java
+++ src/main/java/MainDriver/Playground.java
@@ -5,6 +5,9 @@
  */
 package MainDriver;
 
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jline.terminal.Terminal;
 
 /**
@@ -13,7 +16,7 @@ import org.jline.terminal.Terminal;
  */
 public class Playground {
 
-    public static void charInputPlayground(Terminal terminal) throws Exception {
+    public static void charInputPlayground(Terminal terminal) throws IOException {
         terminal.writer().println(Generic.ConsoleColor.ANSI_F_BRED + "Enter keys to show its integer values");
         terminal.writer().println("Press 'enter' to go to next line");
         while (true) {
