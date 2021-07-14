75f37438716e70b28155a0aac8ab015a7118a269
Tiffceet <zkang.loo@gmail.com>
2021-07-14 16:58:05 +0800

    Make sure karaoke frame is using correct layout

--- src/main/java/MainDriver/KaraokeSessionAddSongDialog.java
+++ src/main/java/MainDriver/KaraokeSessionAddSongDialog.java
@@ -14,10 +14,23 @@ import java.awt.event.WindowEvent;
  */
 public class KaraokeSessionAddSongDialog extends javax.swing.JDialog {
 
+    /**
+     * 
+     * @param parent parent window calling this dialog
+     */
     public KaraokeSessionAddSongDialog(javax.swing.JFrame parent) {
+        // Call JDialog constructor
         super(parent, true);
+        
+        // Prepare page
         initComponents();
+        
+        this.setTitle(parent.getTitle() + " >> Add Song");
+        this.setLocationRelativeTo(parent);
+        
+        // Make sure the window is properly disposed
         this.addWindowListener(new WindowAdapter() {
+            @Override
             public void windowClosing(WindowEvent e) {
                 e.getWindow().dispose();
             }
@@ -34,40 +47,90 @@ public class KaraokeSessionAddSongDialog extends javax.swing.JDialog {
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
 
-        centerPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
         topPanel = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        searchLabel = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
+        searchSongQueryTextField = new javax.swing.JTextField();
+        centerPanel = new javax.swing.JPanel();
         bottomPanel = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         addSongBtn = new javax.swing.JButton();
         cancelBtn = new javax.swing.JButton();
 
+        jLabel1.setText("jLabel1");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+
+        topPanel.setLayout(new java.awt.BorderLayout());
+
+        java.awt.GridBagLayout jPanel3Layout = new java.awt.GridBagLayout();
+        jPanel3Layout.columnWeights = new double[] {1.0};
+        jPanel3Layout.rowWeights = new double[] {1.0};
+        jPanel3.setLayout(jPanel3Layout);
+
+        searchLabel.setText("Search:");
+        searchLabel.setMaximumSize(new java.awt.Dimension(45, 22));
+        searchLabel.setMinimumSize(new java.awt.Dimension(45, 22));
+        searchLabel.setPreferredSize(new java.awt.Dimension(45, 22));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(10, 10, 10, 0);
+        jPanel3.add(searchLabel, gridBagConstraints);
+
+        topPanel.add(jPanel3, java.awt.BorderLayout.WEST);
+
+        java.awt.GridBagLayout jPanel4Layout = new java.awt.GridBagLayout();
+        jPanel4Layout.columnWeights = new double[] {1.0};
+        jPanel4Layout.rowWeights = new double[] {1.0};
+        jPanel4.setLayout(jPanel4Layout);
+
+        searchSongQueryTextField.setToolTipText("Search for songs here !");
+        searchSongQueryTextField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                searchSongQueryTextFieldActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.ipadx = 7;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(10, 10, 10, 10);
+        jPanel4.add(searchSongQueryTextField, gridBagConstraints);
+
+        topPanel.add(jPanel4, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(topPanel, java.awt.BorderLayout.PAGE_START);
 
         javax.swing.GroupLayout centerPanelLayout = new javax.swing.GroupLayout(centerPanel);
         centerPanel.setLayout(centerPanelLayout);
         centerPanelLayout.setHorizontalGroup(
             centerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 471, Short.MAX_VALUE)
         );
         centerPanelLayout.setVerticalGroup(
             centerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 158, Short.MAX_VALUE)
+            .addGap(0, 216, Short.MAX_VALUE)
         );
 
         getContentPane().add(centerPanel, java.awt.BorderLayout.CENTER);
-
-        javax.swing.GroupLayout topPanelLayout = new javax.swing.GroupLayout(topPanel);
-        topPanel.setLayout(topPanelLayout);
-        topPanelLayout.setHorizontalGroup(
-            topPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        topPanelLayout.setVerticalGroup(
-            topPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
-        );
-
-        getContentPane().add(topPanel, java.awt.BorderLayout.PAGE_START);
 
         bottomPanel.setLayout(new java.awt.BorderLayout());
 
@@ -87,6 +150,11 @@ public class KaraokeSessionAddSongDialog extends javax.swing.JDialog {
         jPanel1.add(addSongBtn, gridBagConstraints);
 
         cancelBtn.setText("Cancel");
+        cancelBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cancelBtnActionPerformed(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.insets = new java.awt.Insets(10, 0, 10, 10);
         jPanel1.add(cancelBtn, gridBagConstraints);
@@ -103,6 +171,15 @@ public class KaraokeSessionAddSongDialog extends javax.swing.JDialog {
         this.dispose();
     }//GEN-LAST:event_addSongBtnActionPerformed
 
+    private void cancelBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelBtnActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_cancelBtnActionPerformed
+
+    private void searchSongQueryTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchSongQueryTextFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_searchSongQueryTextFieldActionPerformed
+
     public Object[] run() {
         this.setVisible(true);
         return new Object[]{"Test"};
@@ -113,7 +190,13 @@ public class KaraokeSessionAddSongDialog extends javax.swing.JDialog {
     private javax.swing.JPanel bottomPanel;
     private javax.swing.JButton cancelBtn;
     private javax.swing.JPanel centerPanel;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JLabel searchLabel;
+    private javax.swing.JTextField searchSongQueryTextField;
     private javax.swing.JPanel topPanel;
     // End of variables declaration//GEN-END:variables
 }
