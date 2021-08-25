b4686054f7d2353d62776a19f0aa26ad5a94d3b5
Tiffceet <zkang.loo@gmail.com>
2021-08-25 21:51:12 +0800

    Disable songhistory UI table editable

--- src/main/java/SessionManagement/UI/KaraokeSessionSongAddHistoryDialog.java
+++ src/main/java/SessionManagement/UI/KaraokeSessionSongAddHistoryDialog.java
@@ -57,9 +57,16 @@ public class KaraokeSessionSongAddHistoryDialog extends javax.swing.JDialog {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(songHistoryTable);
